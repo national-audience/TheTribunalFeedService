63a04afdc23c7853fb1271d596e9a5478be18f2c
Rafael Expósito Pérez <r.exposito.2018@gmail.com>
2022-03-27 21:04:11 +0200

    init commit

---
+++ src/main/java/java/io/github/nationalaudience/thetribunal/TheTribunalApplicationFeed.java
@@ -0,0 +1,13 @@
+package java.io.github.nationalaudience.thetribunal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheTribunalApplicationFeed {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheTribunalApplicationFeed.class, args);
+    }
+
+}
---
+++ src/main/java/java/io/github/nationalaudience/thetribunal/service/FeedService.java
@@ -0,0 +1,5 @@
+package java.io.github.nationalaudience.thetribunal.service;
+
+public class FeedService {
+
+}
